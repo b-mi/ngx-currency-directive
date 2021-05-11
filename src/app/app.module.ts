1aaca6cd4ae7d9ba3775287e528e99a12e55af46
code@jasom.eu <site@arxa.eu>
2021-05-11 09:16:36 +0200

    upg mat 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppDashboardComponent } from './app-dashboard/app-dashboard.component';
-import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule,
-        MatFormFieldModule, MatInputModule, MatToolbarModule  } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { SampleComponent } from './sample/sample.component';
 import { CurrencyDirective } from './currency.directive';
 
